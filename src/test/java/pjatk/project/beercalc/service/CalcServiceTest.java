68d86edc6dd900b57072c8e8392d79afcbd1e649
Mateusz Grube <s20156@pjwstk.edu.pl>
2021-02-06 10:31:54 +0100

    Fixes in tests

--- src/test/java/pjatk/project/beercalc/service/CalcServiceTest.java
+++ src/test/java/pjatk/project/beercalc/service/CalcServiceTest.java
@@ -1,8 +1,10 @@
 package pjatk.project.beercalc.service;
 
+import org.assertj.core.data.Percentage;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.springframework.util.Assert;
 import pjatk.project.beercalc.model.Recipe;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -26,6 +28,7 @@ class CalcServiceTest {
         //given
         Recipe recipe = new Recipe("test", 5, 20, 60, 30);
         //when
+        when(recipeRepository.findById(recipe.getId())).thenReturn(Optional.of(recipe));
         calcService.calcExtract(recipe.getId());
         //then
         assertThat(recipe.getExtract()).isEqualTo(12);
@@ -36,6 +39,7 @@ class CalcServiceTest {
         //given
         Recipe recipe = new Recipe("test", 5, 20, 60, 12, 30);
         //when
+        when(recipeRepository.findById(recipe.getId())).thenReturn(Optional.of(recipe));
         calcService.calcIBU(recipe.getId());
         //then
         assertThat(recipe.getIBU()).isGreaterThan(0);
@@ -48,6 +52,6 @@ class CalcServiceTest {
         //when
         recipe = calcService.calcMetricToImperial(recipe);
         //then
-        assertThat(recipe.getExtract()).isEqualTo(26.4);
+        assertThat(recipe.getTargetVolume()).isCloseTo(26.4, Percentage.withPercentage(0.02));
     }
 }
