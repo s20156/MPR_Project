eb73dd98c0f8190e0d3af0892a92e14ba5b6dd7b
Mateusz Grube <s20156@pjwstk.edu.pl>
2021-02-06 17:09:56 +0100

    Added crud functions to RecipeBookController

--- src/main/java/pjatk/project/beercalc/controller/RecipeBookController.java
+++ src/main/java/pjatk/project/beercalc/controller/RecipeBookController.java
@@ -1,14 +1,34 @@
 package pjatk.project.beercalc.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pjatk.project.beercalc.model.Recipe;
 import pjatk.project.beercalc.service.RecipeBookService;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/recipebook")
 public class RecipeBookController {
     private RecipeBookService recipeBookService;
 
+    @GetMapping
+    public ResponseEntity<List<List<Recipe>>> findAll() {
+        List<List<Recipe>> all = recipeBookService.findAll();
+        return ResponseEntity.ok(all);
+    }
 
-
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<List<Recipe>>> findById(@PathVariable Long id) {
+        Optional<List<Recipe>> byId = recipeBookService.findById(id);
+        if (byId.isPresent()) {
+            return ResponseEntity.ok(byId);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
--- src/main/java/pjatk/project/beercalc/service/RecipeBookService.java
+++ src/main/java/pjatk/project/beercalc/service/RecipeBookService.java
@@ -2,7 +2,11 @@ package pjatk.project.beercalc.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pjatk.project.beercalc.model.Recipe;
 import pjatk.project.beercalc.repository.RecipeBookRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RecipeBookService {
@@ -11,5 +15,13 @@ public class RecipeBookService {
     @Autowired
     public RecipeBookService(RecipeBookRepository recipeBookRepository) {
         this.recipeBookRepository = recipeBookRepository;
+    }
+
+    public List<List<Recipe>> findAll() {
+        return recipeBookRepository.findAll();
+    }
+
+    public Optional<List<Recipe>> findById(Long id) {
+        return recipeBookRepository.findById(id);
     }
 }
